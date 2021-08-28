aee89b6e88899206e2f31962a3ad9ac74be0d7bd
Melina Corbalan <melina.corbalan@gmail.com>
2021-08-29 01:16:43 +0200

    updates carrito.js and scss

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,3 @@
-
 const printDetalles = () => {
 
     let detalleCompra = document.querySelector('.detalle-carrito');
@@ -14,7 +13,7 @@ const printDetalles = () => {
             carrito_tour.setAttribute("class", "carrito-tour");
             detalleCompra.appendChild(carrito_tour);
 
-            let detalles_tour = document.createElement ("div");
+            let detalles_tour = document.createElement("div");
             detalles_tour.setAttribute("class", "detalles-tour");
             carrito_tour.appendChild(detalles_tour);
 
@@ -37,23 +36,41 @@ const printDetalles = () => {
             opciones.setAttribute("class", "opciones");
             carrito_tour.appendChild(opciones);
 
-            //BOTON BORRAR TOUR
-            
-            // let button = document.createElement("button");
-            // button.setAttribute("id", "btn-eliminar");
-            // button.setAttribute("type", "button");
-            // button.setAttribute("class", "btn btn-danger");
-            // button.textContent = "Eliminar";
-            // opciones.appendChild(button);
+
+            //BOTON BORRAR TOUR - SIN FUNCIONAR
+
+            let button = document.createElement("button");
+            button.setAttribute("id", "btn-eliminar");
+            button.setAttribute("type", "button");
+            button.setAttribute("class", "btn btn-danger");
+            button.textContent = "Eliminar";
+            opciones.appendChild(button);
         })
+
+        // PRECIO FINAL 
+
+        const precios = printCompra.map(x => x.precio);
+        let sum = 0;
+        for (let i = 0; i < precios.length; i++) {
+            sum += precios[i];
+        }
+
+        let valorTotal = document.createElement("p");
+        valorTotal.setAttribute("class", "valor-total");
+        valorTotal.textContent = `Precio total: ${sum} EUR`
+        detalleCompra.appendChild(valorTotal);
+
+        console.log(sum);
+
+
 
     } else {
         let noTour = document.createElement("p");
         noTour.textContent = "No tienes tours en tu carrito"
         detalleCompra.appendChild(noTour);
     }
+
 }
 
 window.addEventListener("load", printDetalles)
 
-// PRECIO TOTAL COMPRA ?
